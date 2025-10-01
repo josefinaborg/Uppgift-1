db044d48d2fcf21260ff45a9f63d4553d6f040ce
josefinaborg <josefinaborg@outlook.com>
2025-10-01 17:35:46 +0200

    Add files via upload

---
+++ Inlämningsuppgift 1/script.js
@@ -0,0 +1,29 @@
+/*
+alertbox som dyker upp på hemsidan
+alert( "Välkommen" )
+document.write( 456 )
+console.log( 789 )
+
+/*
+
+
+// 1) variabler
+
+let box1 // skapa en variabel som heter box1
+box1 = 5 // lägger in nummer 5 i box1
+box1 = 10 // det som är sist är det som gäller, därmer kommer 10 upp och inte 5 
+
+box1 = 2+2*3
+bo1 = (2+2)*3
+
+// P arentess
+// E xponent
+// M ultiplikation
+// D ivition
+// A ddition
+// S ubtraktion
+
+console.log( box1 ) skriver jag såhär så kommer box 10 upp automatiskt och jag behöver inte söka på det.
+
+
+
